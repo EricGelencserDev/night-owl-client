adb3297ed841c798aafe9c6f6a3b3c07df9ee39f
Eric Gelencser <ericgelencserdev@gmail.com>
2019-01-11 01:11:49 -0800

    added git ignore =)

---
+++ app/api/dummy/brand.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: 'NightOwl Productions',
+  desc: 'Event Managment Software',
+  prefix: 'nightowl',
+  footerText: 'NightOwl Productions All Rights Reserved 2018',
+  logoText: 'NightOwl Productions',
+};
